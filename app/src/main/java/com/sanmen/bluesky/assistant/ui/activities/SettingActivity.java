31eec735c87220ab659d9b45204bc7a3e449a3c3
lxt_bluesky <864464536@qq.com>
2018-10-31 17:33:30 +0800

    添加设置页面

---
+++ app/src/main/java/com/sanmen/bluesky/assistant/ui/activities/SettingActivity.java
@@ -0,0 +1,20 @@
+package com.sanmen.bluesky.assistant.ui.activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
+import com.sanmen.bluesky.assistant.base.BaseActivity;
+
+/**
+ * @author lxt_bluesky
+ * @date 2018/10/31
+ * @description
+ */
+public class SettingActivity extends BaseActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
+}
